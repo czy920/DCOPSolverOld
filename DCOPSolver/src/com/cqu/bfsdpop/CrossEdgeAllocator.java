37d9c954b4210f767a7b896fd18dbb0fc226f8cf
simpher <hezhen127287@163.com>
2014-09-19 17:11:57 +0800

    完成了BFSDPOP中对交叉边约束计算的分配（未测试）

--- DCOPSolver/src/com/cqu/bfsdpop/CrossEdgeAllocator.java
+++ DCOPSolver/src/com/cqu/bfsdpop/CrossEdgeAllocator.java
@@ -1,5 +1,6 @@
 package com.cqu.bfsdpop;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,21 +13,20 @@ public class CrossEdgeAllocator {
 	private Map<Integer, boolean[]> isCrossNeighbours;
 	private Map<Integer, boolean[]> considerCrossConstraint;
 	private Problem problem;
-	private Map<Integer, Map<Integer, Integer>> branchCrossConstraints;
-	private Map<Integer, List<Edge>> subtreeCrossConstraints;
+	private Map<Integer, Map<Integer, List<Edge>>> branchCrossConstraints;
 	
 	public CrossEdgeAllocator(Problem problem) {
 		// TODO Auto-generated constructor stub
 		this.problem=problem;
 		isCrossNeighbours=new HashMap<Integer, boolean[]>();
 		considerCrossConstraint=new HashMap<Integer, boolean[]>();
-		branchCrossConstraints=new HashMap<Integer, Map<Integer, Integer>>();
-		subtreeCrossConstraints=new HashMap<Integer, List<Edge>>();
+		branchCrossConstraints=new HashMap<Integer, Map<Integer, List<Edge>>>();
 		
 		for(Integer agentId : problem.neighbourAgents.keySet())
 		{
 			int[] neighbours=problem.neighbourAgents.get(agentId);
 			boolean[] crossNeighbours=new boolean[neighbours.length];
+			boolean[] considered=new boolean[neighbours.length];
 			Integer parent=problem.parentAgents.get(agentId);
 			int[] children=problem.childAgents.get(agentId);
 			for(int i=0;i<neighbours.length;i++)
@@ -34,12 +34,15 @@ public class CrossEdgeAllocator {
 				if(neighbours[i]==parent||CollectionUtil.indexOf(children, neighbours[i])!=-1)
 				{
 					crossNeighbours[i]=false;
+					considered[i]=true;
 				}else
 				{
 					crossNeighbours[i]=true;
+					considered[i]=false;
 				}
 			}
 			isCrossNeighbours.put(agentId, crossNeighbours);
+			considerCrossConstraint.put(agentId, considered);
 		}
 	}
 	
@@ -54,22 +57,109 @@ public class CrossEdgeAllocator {
 				if(crossNeighbours[i]==true)
 				{
 					Edge edge=new Edge(agentId, neighbours[i]);
-					Integer[] mutualAncestorAndBranches=this.findFirstMutualAncestorAndBranches(edge);
-					
+					this.findFirstMutualAncestor(edge);
+					if(branchCrossConstraints.containsKey(edge.getMutualAncestor())==false)
+					{
+						Map<Integer, List<Edge>> crossConstraints=new HashMap<Integer, List<Edge>>();
+						branchCrossConstraints.put(edge.getMutualAncestor(), crossConstraints);
+					}
+					Map<Integer, List<Edge>> crossConstraints=branchCrossConstraints.get(edge.getMutualAncestor());
+					if(crossConstraints.containsKey(edge.getBranchA())==false)
+					{
+						List<Edge> constraints=new ArrayList<Edge>();
+						crossConstraints.put(edge.getBranchA(), constraints);
+					}
+					if(crossConstraints.containsKey(edge.getBranchB())==false)
+					{
+						List<Edge> constraints=new ArrayList<Edge>();
+						crossConstraints.put(edge.getBranchB(), constraints);
+					}
+					List<Edge> constraints=crossConstraints.get(edge.getBranchA());
+					this.addIfNotExist(constraints, edge);
+					constraints=crossConstraints.get(edge.getBranchB());
+					this.addIfNotExist(constraints, edge);
 				}
+			}
+		}
+		for(Integer agentId : this.branchCrossConstraints.keySet())
+		{
+			Map<Integer, List<Edge>> crossConstraints=branchCrossConstraints.get(agentId);
+			Integer maxKey=this.maxEdgesItemKey(crossConstraints);
+			while(crossConstraints.get(maxKey).size()>0)
+			{
+				for(Edge edge : crossConstraints.get(maxKey))
+				{
+					boolean[] considered=considerCrossConstraint.get(edge.getNodeA());
+					int[] neighbours=problem.neighbourAgents.get(edge.getNodeA());
+					considered[this.indexInNeighbours(neighbours, edge.getNodeA())]=true;
+				}
+				for(Edge edge : crossConstraints.get(maxKey))
+				{
+					this.removeEdge(crossConstraints.get(edge.getBranchB()), edge);
+				}
+				crossConstraints.get(maxKey).clear();
+				maxKey=this.maxEdgesItemKey(crossConstraints);
 			}
 		}
 	}
 	
-	/**
-	 * 
-	 * @param edge
-	 * @return {mutualAncestorNode, branchA(the direct child node of mutualAncestorNode), branchB}
-	 */
-	private Integer[] findFirstMutualAncestorAndBranches(Edge edge)
+	private boolean addIfNotExist(List<Edge> edgeList, Edge target)
 	{
-		Integer branchNodeA=edge.getIdNodeA();
-		Integer branchNodeB=edge.getIdNodeB();
+		for(Edge edge : edgeList)
+		{
+			if(edge.equals(target)==true)
+			{
+				return false;
+			}
+		}
+		edgeList.add(target);
+		return true;
+	}
+	
+	private boolean removeEdge(List<Edge> edgeList, Edge target)
+	{
+		for(Edge edge : edgeList)
+		{
+			if(edge.equals(target)==true)
+			{
+				edgeList.remove(edge);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private Integer indexInNeighbours(int[] neighbours, Integer nodeA)
+	{
+		for(int i=0;i<neighbours.length;i++)
+		{
+			if(nodeA==neighbours[i])
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private Integer maxEdgesItemKey(Map<Integer, List<Edge>> crossConstraints)
+	{
+		int maxCount=0;
+		int maxKey=-1;
+		for(Integer key : crossConstraints.keySet())
+		{
+			if(crossConstraints.get(key).size()>maxCount)
+			{
+				maxCount=crossConstraints.get(key).size();
+				maxKey=key;
+			}
+		}
+		return maxKey;
+	}
+	
+	private void findFirstMutualAncestor(Edge edge)
+	{
+		Integer branchNodeA=edge.getNodeA();
+		Integer branchNodeB=edge.getNodeB();
 		int branchLevelA=problem.agentLevels.get(branchNodeA);
 		int branchLevelB=problem.agentLevels.get(branchNodeB);
 		if(branchLevelA<branchLevelB)
@@ -93,7 +183,7 @@ public class CrossEdgeAllocator {
 			branchNodeB=problem.parentAgents.get(branchNodeB);
 			branchLevelB=problem.agentLevels.get(branchNodeB);
 		}
-		return new Integer[]{branchNodeA, branchNodeABuf, branchNodeBBuf};
+		edge.setMutualAncestorAndBranches(branchNodeA, branchNodeABuf, branchNodeBBuf);
 	}
 	
 	public Map<Integer, boolean[]> getConsideredConstraint()
--- DCOPSolver/src/com/cqu/bfsdpop/Edge.java
+++ DCOPSolver/src/com/cqu/bfsdpop/Edge.java
@@ -2,25 +2,56 @@ package com.cqu.bfsdpop;
 
 public class Edge {
 	
-	private Integer idNodeA;
-	private Integer idNodeB;
+	private Integer nodeA;
+	private Integer nodeB;
+	private Integer mutualAncestor;
+	private Integer branchA;
+	private Integer branchB;
 		
-	public Edge(Integer idNodeA, Integer idNodeB) {
+	public Edge(Integer nodeA, Integer nodeB) {
 		super();
-		this.idNodeA = idNodeA;
-		this.idNodeB = idNodeB;
+		this.nodeA = nodeA;
+		this.nodeB = nodeB;
 	}
-	
-	public Integer getIdNodeA() {
-		return idNodeA;
+
+	public Edge(Integer nodeA, Integer nodeB, Integer mutualAncestor,
+			Integer branchA, Integer branchB) {
+		super();
+		this.nodeA = nodeA;
+		this.nodeB = nodeB;
+		this.mutualAncestor = mutualAncestor;
+		this.branchA = branchA;
+		this.branchB = branchB;
 	}
-	
-	public Integer getIdNodeB() {
-		return idNodeB;
+
+	public Integer getMutualAncestor() {
+		return mutualAncestor;
 	}
-	
+
+	public void setMutualAncestorAndBranches(Integer mutualAncestor, Integer branchA, Integer branchB) {
+		this.mutualAncestor = mutualAncestor;
+		this.branchA=branchA;
+		this.branchB=branchB;
+	}
+
+	public Integer getBranchA() {
+		return branchA;
+	}
+
+	public Integer getBranchB() {
+		return branchB;
+	}
+
+	public Integer getNodeA() {
+		return nodeA;
+	}
+
+	public Integer getNodeB() {
+		return nodeB;
+	}
+
 	public boolean equals(Edge e1) {
-		return (this.idNodeA==e1.idNodeA&&this.idNodeB==e1.idNodeB)||
-				(this.idNodeA==e1.idNodeB&&this.idNodeB==e1.idNodeA);
+		return (this.nodeA==e1.nodeA&&this.nodeB==e1.nodeB)||
+				(this.nodeA==e1.nodeB&&this.nodeB==e1.nodeA);
 	}
 }
