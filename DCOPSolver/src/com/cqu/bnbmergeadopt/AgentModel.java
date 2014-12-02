6820da5291538b14f3e41671f43513a5c0973b3a
heniancheng <845802942@qq.com>
2014-12-02 12:05:11 +0800

    将几个同步算法中的InitSelf()函数作适当调整ID的改变

--- DCOPSolver/src/com/cqu/bnbmergeadopt/AgentModel.java
+++ DCOPSolver/src/com/cqu/bnbmergeadopt/AgentModel.java
@@ -396,9 +396,9 @@ public class AgentModel extends AgentCycle {
 		private void InitSelf(){
 			
 			agent.TH=0;
-			//int oldvalueIndex=this.valueIndex;
+			int oldvalueIndex=agent.valueIndex;
 			agent.valueIndex=agent.computeMinimalLBAndUB()[0];
-			//if(oldvalueIndex!=this.valueIndex||this.valueID==0)
+			if(oldvalueIndex!=agent.valueIndex||agent.valueID==0)
 			agent.valueID = agent.valueID + 1;
 			Debugger.valueChanges.get(agent.name).add(agent.valueIndex);			
 		}
@@ -444,11 +444,11 @@ public class AgentModel extends AgentCycle {
 			agent.currentContext = new Context();
 			
 			if(!agent.isRootAgent())
-				agent.currentContext.addOrUpdate(agent.parent, 0, 0); //仅仅初始化为第1个取值
+				agent.currentContext.addOrUpdate(agent.parent, 0, 1); //仅仅初始化为第1个取值
 			if(agent.pseudoParents!=null)
 			{
 				for(int pseudoP:agent.pseudoParents){
-				    agent.currentContext.addOrUpdate(pseudoP, 0, 0); //仅仅初始化为第1个取值
+				    agent.currentContext.addOrUpdate(pseudoP, 0, 1); //仅仅初始化为第1个取值
 				}
 			}
 
@@ -733,7 +733,7 @@ public class AgentModel extends AgentCycle {
 				agent.valueID = agent.valueID + 1;
 			}
 
-			//System.out.println("agent"+agent.id+": "+agent.valueIndex+"\t"+agent.valueID+"\t"+agent.TH+"\t"+agent.LB+"\t"+agent.UB+"\t"+agent.nccc);
+			System.out.println("agent"+agent.id+": "+agent.valueIndex+"\t"+agent.valueID+"\t"+agent.TH+"\t"+agent.LB+"\t"+agent.UB+"\t"+agent.nccc);
 			this.maintainChildThresholdInvariant();
 			this.maintainAllocationInvariant();   //必须将这个放在发送VALUE信息之前
 			sendValueMessages();
@@ -889,10 +889,10 @@ public class AgentModel extends AgentCycle {
 			agent.valueID = 0;
 			agent.currentContext = new Context();
 			if (!agent.isRootAgent())
-				agent.currentContext.addOrUpdate(agent.parent, 0, 0); // 仅仅初始化为第1个取值
+				agent.currentContext.addOrUpdate(agent.parent, 0, 1); // 仅仅初始化为第1个取值
 			if (agent.pseudoParents != null) {
 				for (int pseudoP : agent.pseudoParents) {
-					agent.currentContext.addOrUpdate(pseudoP, 0, 0); // 仅仅初始化为第1个取值
+					agent.currentContext.addOrUpdate(pseudoP, 0, 1); // 仅仅初始化为第1个取值
 				}
 			}
 			agent.lbs = new HashMap<Integer, int[]>();
@@ -934,9 +934,9 @@ public class AgentModel extends AgentCycle {
 		private void InitSelf() {
 
 			agent.TH = Infinity.INFINITY;
-			// int oldvalueIndex=this.valueIndex;
+			int oldvalueIndex=agent.valueIndex;
 			agent.valueIndex = agent.computeMinimalLBAndUB()[0];
-			// if(oldvalueIndex!=this.valueIndex||this.valueID==0)
+			if(oldvalueIndex!=agent.valueIndex||agent.valueID==0)
 			agent.valueID = valueID + 1;
 			Debugger.valueChanges.get(agent.name).add(agent.valueIndex);
 
@@ -980,7 +980,7 @@ public class AgentModel extends AgentCycle {
 					}
 				}
 			}
-			//System.out.println("agent"+agent.id+": "+agent.valueIndex+"\t"+agent.valueID+"\t"+agent.TH+"\t"+agent.LB+"\t"+agent.UB+"\t"+agent.nccc);
+			System.out.println("agent"+agent.id+": "+agent.valueIndex+"\t"+agent.valueID+"\t"+agent.TH+"\t"+agent.LB+"\t"+agent.UB+"\t"+agent.nccc);
 			if (((isRootAgent() == true) && (agent.UB <= agent.LB))
 					|| agent.Readytermintate == true &&agent.TH==agent.UB) {
 				sendTerminateMessages();
