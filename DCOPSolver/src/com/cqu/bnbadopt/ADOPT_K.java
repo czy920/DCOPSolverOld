88c7831872615bb2134245232daad8ee270ab8a2
heniancheng <845802942@qq.com>
2014-12-10 22:01:46 +0800

    修改AODPT_K

--- DCOPSolver/src/com/cqu/bnbadopt/ADOPT_K.java
+++ DCOPSolver/src/com/cqu/bnbadopt/ADOPT_K.java
@@ -190,12 +190,33 @@ public class ADOPT_K extends AgentCycle {
 		this.increaseNcccLocal();
 		int oldValueIndex = valueIndex;
 		maintainTHInvariant();
-		if(compute[1]>=Math.min(this.TH_B, this.UB))
-			this.valueIndex=compute[0];
-		else if(this.TH_A==this.UB)
-			{this.valueIndex=compute[2];System.out.println("th1=ub");}
-		else if(compute[1]>this.TH_A+(this.K-1))
-			{this.valueIndex=compute[0];System.out.println("lbi>th1");}
+		if (this.Readytermintate) {
+			if (this.TH_A == this.UB) {
+				this.valueIndex = compute[2];
+				//System.out.println(this.name+": "+"th1=ub");
+			} else if (compute[1] > this.TH_A + (this.K - 1)) {
+				this.valueIndex = compute[0];
+				//System.out.println(this.name+": "+"lbi>th1+k");
+			} else if (compute[1] >= Math.min(this.TH_B, this.UB)){
+				this.valueIndex = compute[0];
+				//System.out.println(this.name+": "+"lbi>=min{th2,ub}");
+			}
+		} else {
+			if (this.TH_A == this.UB) {
+				this.valueIndex = compute[2];
+				//System.out.println(this.name+": "+"th1=ub");
+			} else if (compute[1] > this.TH_A + this.K) {
+				this.valueIndex = compute[0];
+				//System.out.println(this.name+": "+"lbi>th1+k");
+			}
+			if (compute[1] >= Math.min(this.TH_B, this.UB)){
+				this.valueIndex = compute[0];
+				//System.out.println(this.name+": "+"lbi>=min{th2,ub}");
+			}
+			if((this.TH_A==this.UB)&&(compute[1]< Math.min(this.TH_B, this.UB))){
+				System.out.println("this is track result!!!");
+			}
+		}
 		if(valueIndex!=oldValueIndex){
 			valueID++;
 			Debugger.valueChanges.get(this.name).add(valueIndex);
@@ -203,7 +224,7 @@ public class ADOPT_K extends AgentCycle {
 		this.maintainChildThresholdInvariant();
 		this.maintainCurrentTHInvariant();
 		this.maintainAllocationInvariant();
-		//System.out.println("agent"+this.id+": "+this.valueIndex+"\t"+this.valueID+"\t"+this.TH1+"\t"+this.TH2+"\t"+this.LB+"\t"+this.UB);
+		System.out.println("agent"+this.id+": "+this.valueIndex+"\t"+this.valueID+"\t"+this.TH_A+"\t"+this.TH_B+"\t"+this.LB+"\t"+this.UB);
 		if(this.TH_A==this.UB)
 			if(this.isRootAgent()||this.Readytermintate==true){
 				sendTerminateMessages();
