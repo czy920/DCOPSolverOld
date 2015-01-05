1436eba09b11216efeff9c756d264ba71884415f
xiangbai <wanglei8568@126.com>
2015-01-05 15:27:51 +0800

    get agentProperty

--- DCOPSolver/src/com/cqu/parser/ProblemParser.java
+++ DCOPSolver/src/com/cqu/parser/ProblemParser.java
@@ -13,6 +13,7 @@ import com.cqu.bfsdpop.CrossEdgeAllocator;
 import com.cqu.core.BFSTree;
 import com.cqu.core.TreeGenerator;
 import com.cqu.heuristics.MostConnectedHeuristic;
+import com.cqu.heuristics.MostConstributionHeuristics;
 import com.cqu.main.DOTrenderer;
 import com.cqu.util.XmlUtil;
 import com.cqu.varOrdering.dfs.DFSgeneration;
@@ -78,8 +79,8 @@ public class ProblemParser {
 		if(parser!=null)
 		{
 			parser.parseContent(problem);
-			this.generateCommunicationStructure(problem);
 			this.generateAgentProperty(problem);
+			this.generateCommunicationStructure(problem);
 			return problem;
 		}else
 		{
@@ -101,10 +102,10 @@ public class ProblemParser {
 		/*
 		 * 需要对其进行排序
 		 */
-		/*for (Map.Entry<Integer, Integer> entry : problem.agentProperty.entrySet())
+		for (Map.Entry<Integer, Integer> entry : problem.agentProperty.entrySet())
 		{
 			System.out.println("key: " + entry.getKey() + ", value: " + entry.getValue());
-		}*/
+		}
 	}
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void generateCommunicationStructure(Problem problem)
@@ -118,8 +119,8 @@ public class ProblemParser {
 		{
 			treeGenerator=new BFSTree(problem.neighbourAgents);
 		}
-		DFSgeneration.setRootHeuristics(new MostConnectedHeuristic(problem));
-		DFSgeneration.setNextNodeHeuristics(new MostConnectedHeuristic(problem));
+		DFSgeneration.setRootHeuristics(new MostConstributionHeuristics(problem));
+		DFSgeneration.setNextNodeHeuristics(new MostConstributionHeuristics(problem));
 		treeGenerator.generate();
 		
 		problem.agentLevels=treeGenerator.getNodeLevels();
