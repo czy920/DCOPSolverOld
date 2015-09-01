d26575dad03a86c5b2d45cc4cccc24cd6e9ffb26
simpher <hezhen127287@163.com>
2015-09-01 20:17:28 +0800

    save

---
+++ DCOPSolver/src/com/cqu/aco/AcoAgent.java
@@ -0,0 +1,40 @@
+package com.cqu.aco;
+
+import java.util.List;
+import java.util.Map;
+
+import com.cqu.core.Agent;
+import com.cqu.core.Message;
+
+public class AcoAgent extends Agent{
+
+	public AcoAgent(int id, String name, int level, int[] domain) {
+		super(id, name, level, domain);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Object printResults(List<Map<String, Object>> results) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String easyMessageContent(Message msg, Agent sender, Agent receiver) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void disposeMessage(Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void messageLost(Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
